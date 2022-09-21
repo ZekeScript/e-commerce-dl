69cc9bc4b40f3d69d6256ced41e6c7339f9e4d37
ezEst <estiga27@gmail.com>
2022-09-21 20:00:15 -0300

    fixing import error

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
-import Navbar from './components/Navbar/Navbar';
-import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import Navbar from './components/Navbar/Navbar.js';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer.js';
 
 function App() {
 	return (
